855e33e2bbb05885f530fb2516b22646d44526b2
Eduardo Martins <development@forwwward.studio>
2026-07-09 16:19:48 +0100

    Refactor swiper initialization for consistency and improved readability

--- js/swiper.js
+++ js/swiper.js
@@ -3,42 +3,37 @@ export function swiperInit() {
     console.log("running swiper.js");
 
     // Init Homepage Works Swiper
-    let hpWorksSwiper = document.querySelector('.hp_works_swiper');
-    const hpWorkSwiperSlides = document.querySelectorAll('.hp_works_slide');
+    let hpWorksSwiper = document.querySelector('.is-slider-instructors');
+    const hpWorkSwiperSlides = document.querySelectorAll('.swiper-slide');
 
     if (hpWorksSwiper && hpWorkSwiperSlides) {
 
         setTimeout(() => {
-            hpWorksSwiper = new Swiper('.hp_works_swiper', {
-                slidesPerView: 1.8,
+            hpWorksSwiper = new Swiper('.is-slider-instructors', {
+                slidesPerView: 1.25,
                 spaceBetween: 16,
-                centeredSlides: true,
+                //centeredSlides: true,
                 direction: 'horizontal',
                 loop: true,
                 autoWidth: true,
                 speed: 1000,
-                autoplay: {
-                    delay: 1800,
-                    disableOnInteraction: false,
-                },
+                // autoplay: {
+                //     delay: 1800,
+                //     disableOnInteraction: false,
+                // },
 
                 freeMode: true,
                 freeModeMomentum: false,
                 allowTouchMove: true,
 
                 breakpoints: {
+                    // for screens 500px wide and up
+                    500: {
+                        slidesPerView: 2.25,
+                    },
                     // for screens 768px wide and up
                     768: {
-                        autoplay: {
-                            delay: 0,
-                            disableOnInteraction: false, // if true, will pause on hover
-                        },
-                        slidesPerView: 6,
-                        spaceBetween: 32,
-                        speed: 8000, // Smooth transition speed
-                        centeredSlides: false,
-                        freeMode: true,
-                        freeModeMomentum: false,
+                        slidesPerView: 3.25,
                     }
                 },
                 on: {
